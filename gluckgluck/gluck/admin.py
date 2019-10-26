1c92472b694311856b9cdb92a46770a7e3e946db
GuidaGG <guida.gribeiro@gmail.com>
2019-10-26 20:01:12 +0200

    DID IT WORK

--- gluckgluck/gluck/admin.py
+++ gluckgluck/gluck/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Page, Component, Section
+
+# Register your models here.
+
+class ComponentInline(admin.TabularInline):
+    model = Component
+    min_num = 1
+    can_delete = True
+    extra = 0
+
+
+class SectionInline(admin.TabularInline):
+    model = Section
+    min_num = 1
+    can_delete = True
+    extra = 0
+
+
+class PageAdmin(admin.ModelAdmin):
+    inlines = [
+        SectionInline,
+        ComponentInline,
+    ]
+
+class SectionAdmin(admin.ModelAdmin):
+    inlines = [
+        ComponentInline,
+    ]
+
 
 class EventAdmin(admin.ModelAdmin):
     exclude = ('active', 'published_date')
 
 admin.site.register(Event, EventAdmin)
-
-# Register your models here.
+admin.site.register(Page, PageAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Component)
 
--- gluckgluck/gluck/models.py
+++ gluckgluck/gluck/models.py
@@ -3,6 +3,24 @@ from django.utils import timezone
 
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(default="new page", max_length=300)
+    
+    def __str__(self):
+        return self.title
+
+class Section(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, default=None)
+    section_title = models.TextField(null=False)
+    def __str__(self):
+        return self.section_title
+
+class Component(models.Model):
+    page = models.ForeignKey(Section, on_delete=models.CASCADE, default=None)
+    text = models.TextField(null=False)
+    def __str__(self):
+        return self.text
+
 
 class Event(models.Model):
     title = models.CharField(max_length=300)
